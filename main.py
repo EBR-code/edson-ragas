06b0546d52722904b7da35dfa9b4aaba524b4076
Edson Ragas <ragasedson@gmail.com>
2021-08-26 22:35:49 +0800

    Fix environ

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "os.environ.get("SECRET_KEY")"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 ckeditor = CKEditor(app)
 Bootstrap(app)
@@ -24,8 +24,8 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##SMTP SETUP
-BLOG_EMAIL = "os.environ.get("BLOG_EMAIL")"
-BLOG_PW = "os.environ.get("BLOG_PW")"
+BLOG_EMAIL = os.environ.get("BLOG_EMAIL")
+BLOG_PW = os.environ.get("BLOG_PW")
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")

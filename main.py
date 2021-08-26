ceb413d7510eb6c8731d1f4f68f1d5cf64062f5f
Edson Ragas <ragasedson@gmail.com>
2021-08-26 12:25:26 +0800

    Fix secret key for local host

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+# app.config['SECRET_KEY'] = "os.environ.get("SECRET_KEY")"
+app.config['SECRET_KEY'] = "hkjashdfjkghasjkdfhasjkldhfkl;asdjfhlkjh"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)

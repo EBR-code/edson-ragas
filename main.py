2ff62d13efd435de7a0cc62fd373edb0564af1f7
Edson Ragas <ragasedson@gmail.com>
2021-08-27 19:07:02 +0800

    Hotfix for main.py

--- main.py
+++ main.py
@@ -22,12 +22,21 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
+gravatar = Gravatar(
+    app,
+    size=100,
+    rating="g",
+    default="retro",
+    force_default=False,
+    force_lower=False,
+    use_ssl=False,
+    base_url=None,
+)
 
 ##SMTP SETUP
 BLOG_EMAIL = os.environ.get("BLOG_EMAIL")
